cf47fe4219640e15ca29678d2af7a600a0a1f333
Hugo <hugo@Hugos-Mac.local>
2014-11-26 13:56:42 -0800

    Initial Commit

---
+++ Challenge1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Challenge1
+//
+//  Created by Hugo on 11/26/14.
+//  Copyright (c) 2014 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Challenge1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Challenge1
+//
+//  Created by Hugo on 11/26/14.
+//  Copyright (c) 2014 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
